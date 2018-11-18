bd18ca472384623c6ae9a777792bf173af6c4ee5
BoenYang <yang_7274@126.com>
2018-11-18 12:11:38 +0800

    框架空文件添加

---
+++ Assets/CGF/Utils/StringUtils.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StringUtils {
+
+    public static string FormatStringLength(string str, int limitLen) {
+        return "";
+    }
+
+    public static bool HasChinese(string str) {
+        return false;
+    }
+
+    public static bool CheckAccount() {
+        return false;
+    }
+
+    public static bool CheckPassword() {
+        return false;
+    }
+
+    public static bool HasWhiteSpace() {
+        return false;
+    }
+
+    public static bool GetLength() {
+        return false;
+    }
+
+    public static string FormatNum(int num) {
+        return "";
+    }
+
+}
